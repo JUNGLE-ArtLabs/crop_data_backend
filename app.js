66204fd33f65209359da186d13b5ae9819c2d461
Unju Shizen Nouen <unjushizennouen@gmail.com>
2025-03-23 04:25:27 +0900

    Update app.js for deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 const web3 = new Web3("https://rpc-amoy.polygon.technology");
 
-const contractAddress = "0xecAdfbe8a7e32d9f0793433823b0f753067F91D6" // REMIXのデプロイしたスマートコントラクトのアドレス
+const contractAddress = "0xC766775af3a9e5a46656216EB43f39b419dC9A63" // REMIXのデプロイしたスマートコントラクトのアドレス
 
 const contractABI = [
 		{
